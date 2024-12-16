fc5975a32d281eeb534126aae4b1974412cf7a75
Amro <elrefaei@sheridancollege.ca>
2024-12-15 22:31:40 -0500

    8

--- resource.py
+++ resource.py
@@ -1,7 +1,11 @@
-import json
-
 class Book:
 
  def __init__(self, id: str, title: str, author: str, year: int):
 
     self.id = id
+    self.title = title
+    self.author = author
+    self.year = year
+def __str__(self):
+
+    return f"{self.id}: {self.title} by {self.author} ({self.year})"
