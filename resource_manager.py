822b47e3bc5d35f8ff9817be32850235c04ef2a2
Amro <elrefaei@sheridancollege.ca>
2024-12-15 22:21:28 -0500

    2

--- resource_manager.py
+++ resource_manager.py
@@ -6,4 +6,5 @@ class ResourceManager:
 def create_resource(self, id: str, title: str, author: str, year: int):
    book = Book(id, title, author, year)
 
-    self.resources.append(book)
+def read_resource(self, id: str):
+
