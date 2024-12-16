ad9f59d45ff8dbf19effbeebacfd32b073ef4f8f
Amro <elrefaei@sheridancollege.ca>
2024-12-15 22:26:05 -0500

    33

--- resource_manager.py
+++ resource_manager.py
@@ -7,4 +7,9 @@ def create_resource(self, id: str, title: str, author: str, year: int):
    book = Book(id, title, author, year)
 
 self.resources.append(book)
+def read_resource(self, id: str):
 
+    return next((book for book in self.resources if book.id == id), None)
+def update_resource(self, id: str, title: str, author: str, year: int):
+
+    book = self.read_resource(id)
