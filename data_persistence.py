31c2f757e84a1de4e859b65bf8fb0f9a8c575f52
Amro <elrefaei@sheridancollege.ca>
2024-12-15 22:49:07 -0500

    3333

--- data_persistence.py
+++ data_persistence.py
@@ -3,3 +3,16 @@ import json
 from resource import Book
 
 class DataPersistence:
+    @staticmethod
+def save_data(resources, filename : str):
+    with open(filename, 'w') as file:
+        json.dump([book.__dict__ for book in resources], file)
+
+@staticmethod
+def load_data(filename : str):
+    try:
+        with open(filename, 'r') as file:
+            return [Book(**data) for data in json.load(file)]
+    except FileNotFoundError:
+        return []
+Copy
---
+++ ui.py
@@ -0,0 +1,7 @@
+
+
+from data_persistence import DataPersistence
+
+from resource_manager import ResourceManager
+
+class UserInterface:
